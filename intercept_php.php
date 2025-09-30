748ca3e4e2237034c6b733a6b1b2e4926e94161b
Dario Cangialosi a.k.a. Arkenidar <dario.cangialosi@gmail.com>
2025-09-30 10:44:46 +0200

    [clean] use "str_starts_with" simply . [ AI : ] refactor: simplify PHP file inclusion logic in intercept_php.php

--- intercept_php.php
+++ intercept_php.php
@@ -4,12 +4,9 @@
 
 <link rel="stylesheet" href="style.css">
 <div>
-<?php
-function substr_startswith($haystack, $needle) {
-    return substr($haystack, 0, strlen($needle)) === $needle;
-}
-// include php file with a security restriction
-$path=realpath($_REQUEST["file"].".php");
-if(substr_startswith($path,realpath("."))) include_once($path);
-?>
+    <?php
+    // include php file with a security restriction
+    $path = realpath($_REQUEST["file"] . ".php");
+    if (str_starts_with($path, realpath("."))) include_once($path);
+    ?>
 </div>
